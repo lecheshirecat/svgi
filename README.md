7bcdcbdc80a1e01a42861f4db6d14e0b80fe1d5b
Charlie <contact@pixeliste.fr>
2024-04-01 14:21:50 +0200

    Version 2.0.0 - Build in both CommonJS and ES Module

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ npm install @cheshirecat/svgi --save
 
 ## How to use
 
-Default use in your main.js project
+Default use in your `main.js` project
 
 ```typescript
 import { addFamily } from '@cheshirecat/svgi'
@@ -25,7 +25,7 @@ addFamily('ico', fa)
 
 ## JSON files
 
-The JSON value must be an array of three parameters: width (number), height (number), paths (string). You can separate multiple paths with '|'
+The JSON value can be an array with three parameters: `[width: number, height: number, paths: string]`; or an array with two parameters: `[[xmin: number, ymin: number, width: number, height: number], paths: string]`. You can separate multiple paths with '|'
 
 ```javascript
 {
@@ -33,6 +33,10 @@ The JSON value must be an array of three parameters: width (number), height (num
     256,
     512,
     "M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
+  ],
+  "home": [
+    [0, -960, 960, 960],
+    "M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"
   ],
   ...
 }
@@ -55,8 +59,19 @@ The rendered SVG element can then be easily customized with CSS.
 
 ```html
 ...
-<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="ico ico-arrow-right" data-name="arrow-right" role="img" viewBox="0 0 256 512">
-  <path fill="currentColor" class="path path-1" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" />
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+  class="ico ico-arrow-right"
+  data-name="arrow-right"
+  role="img"
+  viewBox="0 0 256 512"
+>
+  <path
+    fill="currentColor"
+    class="ico-path ico-path-1"
+    d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
+  />
 </svg>
 ...
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,98 @@
 /**
  * SVG Icon Maker
  *
- * @version 1.0.0
- * @author Charlie LEDUC <contact@graphique.io>
+ * @version 2.0.0
+ * @author Charlie LEDUC <contact@pixeliste.fr>
  */
 
-export interface IconsSet {
-    [name: string]: (string | number)[]
-}
+export type IconValues = (string | number | number[])[];
 
 export interface IconObj {
-    width: number
-    height: number
-    paths: string[]
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  paths: string[];
 }
 
-const _SVGI_FAMILIES_: { [name: string]: IconsSet } = {}
+const _SVGI_FAMILIES_: Record<string, Record<string, IconValues>> = {};
 
-export function addFamily(name: string, set: IconsSet): void {
-    if (!name.length) return
-    _SVGI_FAMILIES_[name] = set
+export function addFamily(name: string, set: Record<string, IconValues>): void {
+  if (!name.length) return;
+  _SVGI_FAMILIES_[name] = set;
 }
 
-export function getFamilies(): { [name: string]: IconsSet } {
-    return _SVGI_FAMILIES_
+export function getFamilies(): Record<string, Record<string, IconValues>> {
+  return _SVGI_FAMILIES_;
 }
 
-export function getFamily(name: string): IconsSet | undefined {
-    return _SVGI_FAMILIES_[name]
+export function getFamily(
+  name: string
+): Record<string, IconValues> | undefined {
+  return _SVGI_FAMILIES_[name];
 }
 
 export function getIcon(family: string, name: string): IconObj {
-    const set = _SVGI_FAMILIES_[family]
-    if (set !== undefined) {
-        const values: (string | number)[] = set[name] ?? [0, 0, '']
-        return {
-            width: typeof values[0] === 'number' ? values[0] : 0,
-            height: typeof values[1] === 'number' ? values[1] : 0,
-            paths: typeof values[2] === 'string' ? values[2].split('|') : []
-        }
-    }
+  const set = _SVGI_FAMILIES_[family];
 
-    return {
-        width: 0,
-        height: 0,
-        paths: []
-    }
+  const values: IconValues = set[name] ?? [0, 0, ""];
+  const startval = values.length ? values[0] : 0;
+  const midval = values.length ? values[1] : 0;
+  const endval = values.length > 2 ? values[2] : midval;
+  const strval =
+    typeof endval === "string"
+      ? endval ?? ""
+      : typeof midval === "string"
+      ? midval ?? ""
+      : "";
+
+  const isStartArray = Array.isArray(startval);
+  const xmin = isStartArray && startval.length ? startval[0] ?? 0 : 0;
+  const ymin = isStartArray && startval.length > 1 ? startval[1] ?? 0 : 0;
+  const width =
+    isStartArray && startval.length > 2
+      ? startval[2] ?? 0
+      : typeof startval === "number"
+      ? startval ?? 0
+      : 0;
+  const height =
+    isStartArray && startval.length > 3
+      ? startval[3] ?? 0
+      : typeof endval === "number"
+      ? endval ?? 0
+      : 0;
+  const paths = strval.split("|");
+
+  return {
+    x: xmin,
+    y: ymin,
+    width: width,
+    height: height,
+    paths: paths
+  };
 }
 
-export function makeIcon(family: string, name: string, classes?: string[]) {
-    const icon = getIcon(family, name)
-    const classList = [family, `${family}-${name}`]
-    if (classes && classes.length) {
-        classList.push(...classes)
-    }
-    return `<svg xmlns="http://www.w3.org/2000/svg" class="${classList.filter((e) => e.length).join(' ')}" data-name="${name}" role="img" aria-hidden="true" viewBox="${`0 0 ${icon.width} ${icon.height}`}">${icon.paths.map((path, p) => `<path fill="currentColor" class="${['path', `path-${p + 1}`].join(' ')}" d="${path}" />`)}</svg>`
+export function makeIcon(
+  family: string,
+  name: string,
+  customClasses?: string[]
+): string {
+  const icon = getIcon(family, name);
+  const classList = [family, `${family}-${name}`];
+  if (customClasses?.length) {
+    classList.push(...customClasses);
+  }
+  const classNames = classList.filter((e) => e.length).join(" ");
+  const viewBox = [icon.x, icon.y, icon.width, icon.height].join(" ");
+
+  const elements: string[] = [
+    `<svg xmlns="http://www.w3.org/2000/svg" class="${classNames}" data-name="${name}" role="img" aria-hidden="true" viewBox="${viewBox}">`,
+    ...icon.paths.map((path, p) => {
+      const pathClass = [`${family}-path`, `${family}-path-${p + 1}`].join(" ");
+      return `<path fill="currentColor" class="${pathClass}" d="${path}" />`;
+    }),
+    "</svg>"
+  ];
+
+  return elements.join("");
 }
